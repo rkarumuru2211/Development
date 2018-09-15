3119979c0e75dfb210948adc96e5c113cabfe968
rkarumuru2211 <rakeshkarumuru@gmail.com>
2018-09-16 02:42:25 +0530

    changes made

--- Angular/HelloWorld/src/app/app.module.ts
+++ Angular/HelloWorld/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
