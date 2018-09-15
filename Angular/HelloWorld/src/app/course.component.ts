9b27e9ba109da405765e2402443524c0996aa3ba
rkarumuru2211 <rakeshkarumuru@gmail.com>
2018-09-16 03:02:57 +0530

    commited

--- Angular/HelloWorld/src/app/course.component.ts
+++ Angular/HelloWorld/src/app/course.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core'
 @Component({
     selector:"courses",
-    template:"<h2>Courses</h2>"
+    template:"<h2>{{ title }}</h2>"
 })
 export  class CourseComponent{
+    title='List of Courses';
 
 } 
